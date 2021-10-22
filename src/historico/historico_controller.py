a2bc6c3825a08a3942a02a83e042bfd0bedfd5d7
Lucas <lucasoliveira2306@gmail.com>
2021-10-22 11:14:13 -0300

    Adicionado paginacao de historico.
    Co-Authored-By: jvsdurso <69814362+jvsdurso@users.noreply.github.com>

--- src/historico/historico_controller.py
+++ src/historico/historico_controller.py
@@ -12,13 +12,14 @@ class HistoricoController:
     __ACAO__ = 'history'
     __bot_util__ = BotUtils.get_instance()
     __historico_escolhido__ = ''
+    __nome_historico_selecionado = ''
 
     bot_controller = BotController.get_instance()
 
     def get_historico(self, params):
         resultado = self.__bot_util__.send_get(path=f'/historico/{params}')
-        historico_json = json.loads(resultado.content)['results']
-        return self.converte_historico_json_to_string(historico_json=historico_json)
+        historico_json = json.loads(resultado.content)
+        return historico_json
 
     def converte_historico_json_to_string(self, historico_json):
         historico = ''
@@ -38,6 +39,8 @@ class HistoricoController:
         chat_id = call.message.chat.id
 
         dados = json.loads(call.data)
+        self.__nome_historico_selecionado = dados['nome']
+
         if self.__historico_escolhido__ == 'loja':
             loja_id = dados['id']
             params = f'?loja_id={loja_id}'
@@ -45,12 +48,7 @@ class HistoricoController:
             personagem_id = dados['id']
             params = f'?personagem_id={personagem_id}'
 
-        nome = dados['nome']
-        historico = f'*Histórico de __{nome}__*\n'
-        historico += self.get_historico(params=params)
-
-        self.bot_controller.bot.delete_message(message_id=call.message.id, chat_id=chat_id)
-        self.bot_controller.bot.send_message(chat_id=chat_id, text=historico, parse_mode='MarkdownV2')
+        self.__build_mensagem(params=params, chat_id=chat_id, message_delete_id=call.message.id)
 
     def inicia_interacao_historico(self, message):
         self.__bot_util__.set_uid_telegram(message.from_user.id)
@@ -97,3 +95,33 @@ class HistoricoController:
                                                        reply_markup=botoes)
         self.bot_controller.bot.register_callback_query_handler(func=lambda call: message.id == call.message.id,
                                                                 callback=self.mostra_historico)
+
+    def __build_mensagem(self, params, chat_id, message_delete_id):
+        historico_json = self.get_historico(params=params)
+
+        dados_historico = self.converte_historico_json_to_string(historico_json=historico_json['results'])
+        message_historico = f'*Histórico de __{self.__nome_historico_selecionado}__*\n' \
+                            f'{dados_historico}'
+
+        botoes = []
+        if 'previous' in historico_json and historico_json['previous'] is not None:
+            previous_link = historico_json['previous'].split('?')[1]
+            botoes.append(InlineKeyboardButton(text='\u23EA', callback_data=previous_link))
+        if 'next' in historico_json and historico_json['next'] is not None:
+            next_link = historico_json['next'].split('?')[1]
+            botoes.append(InlineKeyboardButton(text='\u23E9', callback_data=next_link))
+
+        markup = None
+        if len(botoes) > 0:
+            markup = InlineKeyboardMarkup(row_width=2, keyboard=[botoes])
+
+        self.bot_controller.bot.delete_message(message_id=message_delete_id, chat_id=chat_id)
+        message = self.bot_controller.bot.send_message(chat_id=chat_id, text=message_historico,
+                                                       parse_mode='MarkdownV2', reply_markup=markup)
+        self.bot_controller.bot.register_callback_query_handler(func=lambda call: message.id == call.message.id,
+                                                                callback=self.__handle_paginacao__)
+
+    def __handle_paginacao__(self, call):
+        url_params = f'?{call.data}'
+        chat_id = call.message.chat.id
+        self.__build_mensagem(params=url_params, chat_id=chat_id, message_delete_id=call.message.id)
